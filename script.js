6d23edf66c7632e349bcd9e0ed49682bd2b231d6
yahya-farkli <yahyafarkli63@gmail.com>
2024-09-07 21:16:02 +0100

    Add Cards.

--- script.js
+++ script.js
@@ -214,3 +214,50 @@ document.addEventListener('DOMContentLoaded', function() {
       form.addEventListener('submit', handleFormSubmission);
   }
 });
+/* console.clear();
+
+const cardsContainer = document.querySelector(".price-cards");
+const cardsContainerInner = document.querySelector(".price-cards .cards__inner");
+const cards = Array.from(document.querySelectorAll(".price-card"));
+const overlay = document.querySelector(".price-cards .overlay");
+
+const applyOverlayMask = (e) => {
+  const overlayEl = e.currentTarget;
+  const x = e.pageX - cardsContainer.offsetLeft;
+  const y = e.pageY - cardsContainer.offsetTop;
+
+  overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`;
+};
+
+const createOverlayCta = (overlayCard, ctaEl) => {
+  const overlayCta = document.createElement("div");
+  overlayCta.classList.add("price-cta");
+  overlayCta.textContent = ctaEl.textContent;
+  overlayCta.setAttribute("aria-hidden", true);
+  overlayCard.append(overlayCta);
+};
+
+const observer = new ResizeObserver((entries) => {
+  entries.forEach((entry) => {
+    const cardIndex = cards.indexOf(entry.target);
+    let width = entry.borderBoxSize[0].inlineSize;
+    let height = entry.borderBoxSize[0].blockSize;
+
+    if (cardIndex >= 0) {
+      overlay.querySelector('.cards__inner').children[cardIndex].style.width = `${width}px`;
+      overlay.querySelector('.cards__inner').children[cardIndex].style.height = `${height}px`;
+    }
+  });
+});
+
+const initOverlayCard = (cardEl) => {
+  const overlayCard = document.createElement("div");
+  overlayCard.classList.add("price-card");
+  createOverlayCta(overlayCard, cardEl.lastElementChild);
+  overlay.querySelector('.cards__inner').append(overlayCard);
+  observer.observe(cardEl);
+};
+
+cards.forEach(initOverlayCard);
+document.body.addEventListener("pointermove", applyOverlayMask);
+ */
